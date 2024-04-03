865ddf42b733ebd1644644d6aa2532a6e700378a
Axyu01 <adriansosniak01@gmail.com>
2024-04-03 21:57:02 +0200

    changed moving object and  player to suit rigidbody

--- src/objects/Player.gd
+++ src/objects/Player.gd
@@ -1,8 +1,8 @@
-extends CharacterBody2D
+extends RigidBody2D
 class_name Player
 
 @export var max_speed := 600.0
-@export var jump_velocity := -600.0
+@export var jump_impulse := -600.0
 @export var acceleration := 3000.0
 
 @export var jump_gravity_multiplier := 0.6
@@ -14,6 +14,7 @@ class_name Player
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var grab_point: Node2D = $GrabPoint
 @onready var interaction_area: Area2D = $InteractionArea
+@onready var jump_area: Area2D = $JumpArea
 
 var _player_interactables := []
 var _grabbed_interactable = null
@@ -34,9 +35,9 @@ func _calculate_gravity() -> Vector2:
 	var gravity := get_gravity()
 	var is_jumping = Input.is_action_pressed("Jump")
 
-	if is_jumping and velocity.y < -0.01:
+	if is_jumping and linear_velocity.y < -0.01:
 		gravity *= jump_gravity_multiplier
-	elif velocity.y < -0.01:
+	elif linear_velocity.y < -0.01:
 		gravity *= fall_gravity_multiplier
 	
 	return gravity
@@ -54,6 +55,8 @@ func _on_body_exited_interaction_area(body: Node2D) -> void:
 
 func _interact() -> void:
 	if _grabbed_interactable != null:
+		if(_grabbed_interactable is RigidBody2D):
+			mass -= _grabbed_interactable.mass
 		_grabbed_interactable.unfix_position()
 		_grabbed_interactable.off_interaction()
 		_grabbed_interactable = null
@@ -68,6 +71,8 @@ func _interact() -> void:
 				nearest_interactable = interactable
 		
 		_grabbed_interactable = nearest_interactable
+		if(_grabbed_interactable is RigidBody2D):
+			mass += _grabbed_interactable.mass
 		_grabbed_interactable.on_interaction()
 	
 
@@ -86,19 +91,19 @@ func _process(delta: float) -> void:
 		_interact()
 
 	if _grabbed_interactable != null:
-		_grabbed_interactable.fix_position(grab_point.global_position, velocity)
+		_grabbed_interactable.fix_position(grab_point.global_position, linear_velocity)
 
 	_animate_sprite()
 
 
 func _animate_sprite() -> void:
 	var time = Time.get_ticks_msec() / 1000.0
-	var vel_ratio = inverse_lerp(0, max_speed, abs(velocity.x))
+	var vel_ratio = inverse_lerp(0, max_speed, abs(linear_velocity.x))
 	var anim_speed = lerp(animation_speed_idle, animation_speed_full_speed, vel_ratio)
    
-	if velocity.x < -0.01:
+	if linear_velocity.x < -0.01:
 		_facing_right = false
-	elif velocity.x > 0.01:
+	elif linear_velocity.x > 0.01:
 		_facing_right = true
 
 	if time - _last_frame_change_time > 1.0 / anim_speed:
@@ -110,23 +115,30 @@ func _animate_sprite() -> void:
 	
 
 func _physics_process(delta: float) -> void:
+	var impulse = linear_velocity * mass
 	if not is_on_floor():
-		velocity += _calculate_gravity() * delta
+		impulse += _calculate_gravity() * delta * mass
 		
 	if _apply_impulse_velocity:
-		velocity = _impulse_velocity
+		impulse = _impulse_velocity * mass
 		_apply_impulse_velocity = false
 
 	if Input.is_action_just_pressed("Jump") and _controls_enabled and is_on_floor():
-		velocity.y = jump_velocity
+		impulse.y = jump_impulse
 
 	var direction := Input.get_axis("MoveLeft", "MoveRight")
 	if direction and _controls_enabled:
-		velocity.x = move_toward(velocity.x, direction * max_speed, acceleration * delta)
+		impulse.x = move_toward(impulse.x, direction * max_speed * mass, acceleration * delta * mass)
 	else:
-		velocity.x = move_toward(velocity.x, 0, acceleration * delta)
-
-	move_and_slide()
+		impulse.x = move_toward(impulse.x, 0, acceleration * delta * mass)
+	apply_central_impulse(impulse - linear_velocity * mass)
+	#move_and_slide()
+	
+func is_on_floor() -> bool:
+	#print_debug(jump_area.get_overlapping_bodies().size())
+	if(jump_area.get_overlapping_bodies().size()>1):
+		return true
+	return false
 
 
 func apply_impulse_velocity(vel: Vector2) -> void:
--- src/sosivo/scripts/MovingObject.gd
+++ src/sosivo/scripts/MovingObject.gd
@@ -1,15 +1,13 @@
 extends ObjectInteractable
 class_name MovingObject
-
-@export var move_speed : float = 100
-
-var _origin : Vector2
-var _destination : Vector2
-@export var DestinationDelta : Vector2
-
-@export var move_on_start: bool = false
-
-var _originFlag : bool = true
+@export var rb: RigidBody2D
+@export var DestinationDelta: Vector2 = Vector2.ZERO
+@export var maxStrength: float = 0
+@export var maxSpeed = 0
+var _origin : Vector2 = Vector2.ZERO
+var _destination : Vector2 = Vector2.ZERO
+@export var _originFlag : bool = true
+var _timeSinceSwitch :float = 0;
 
 func OnInteraction() -> void:
 	# print(str(self) + " ON!")
@@ -19,38 +17,30 @@ func OffInteraction() -> void:
 	# print(str(self) + " OFF!")
 	_originFlag = true
 	
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_origin = position
-	_destination = position + DestinationDelta
-	
-	_originFlag = not move_on_start
+	_destination = _origin + DestinationDelta
+	pass # Replace with function body.
 
-var velocity : Vector2 = Vector2.ZERO
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta : float) -> void:
-	#const closeToZero : float = 1
-	#var desiredRelocation : Vector2 = Vector2.ZERO
-#
-	#if _originFlag:
-		#desiredRelocation = _origin - position
-	#else:
-		#desiredRelocation = _destination - position
-
-	#if desiredRelocation.length() > closeToZero:
-		#position = lerp(position, position + desiredRelocation.normalized(), _acceleration * delta)
-	#else:
-		#position = lerp(position, desiredRelocation, _acceleration * delta)
-		#transform.position = (Vector2)transform.position + desiredRelocation;
-		
-	var target := _origin if _originFlag else _destination
-	var move_dir = abs((target - position).normalized())
-	position = Vector2(move_toward(position.x, target.x, move_dir.x * move_speed * delta), move_toward(position.y, target.y, move_dir.y * move_speed * delta))
-		
+func _physics_process(delta: float) -> void:		
+	var destination : Vector2 = Vector2.ZERO
+	if(_originFlag):
+		modulate = Color.RED
+		destination = _origin
+	else:
+		modulate = Color.GREEN
+		destination = _destination		
 	
+	var desiredOffset =(destination-position)
+	var desiredMomentum = (desiredOffset*rb.mass/delta);
+	if(desiredMomentum.length() > maxSpeed * rb.mass):
+		desiredMomentum = desiredMomentum.normalized() * maxSpeed * rb.mass
+	var desiredImpulse = (desiredMomentum-rb.linear_velocity*rb.mass)
+	if(desiredImpulse.length() > maxStrength):
+		desiredImpulse = desiredImpulse.normalized()*maxStrength
 	
-#func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
-	#_rb.linear_velocity = velocity
-	#transform.position = (Vector2)transform.position + desiredRelocation;
+	rb.apply_central_impulse(desiredImpulse)
+	pass
